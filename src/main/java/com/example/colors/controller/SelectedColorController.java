6615a632e582546b7ac9aab7f586319e94f53a04
Katarzyna <noworyta.katarzyna@wp.pl>
2023-07-04 10:36:02 +0200

    select view

--- src/main/java/com/example/colors/controller/SelectedColorController.java
+++ src/main/java/com/example/colors/controller/SelectedColorController.java
@@ -1,17 +1,32 @@
 package com.example.colors.controller;
 
+import com.example.colors.dao.SelectedColorDao;
+import com.example.colors.model.SelectedColor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Date;
 
 @Controller
 public class SelectedColorController {
     @Value("${app.title.select}")
     private String title;
 
-    @GetMapping("/select")
-    public String selectedColorPage(Model model){
+    @Autowired
+    SelectedColorDao selectedColorDao;
+
+    @GetMapping({"/select", "/select/{color}"})
+    public String selectedColorPage(@PathVariable(required = false) String color, Model model){
+
+        if(!StringUtils.isEmpty(color)){
+            selectedColorDao.save(new SelectedColor(color, new Date()));
+        }
+
         String[][] colors = {
                 {"red", "blue", "purple", "teal"},
                 {"black","orange", "yellow", "green"},
